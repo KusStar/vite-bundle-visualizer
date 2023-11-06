73410a5bd71b4c261296fc1d70f6274b84425ed3
Kusstar <kussssss@outlook.com>
2023-11-06 17:43:28 +0800

    chore: fix demo/react script

--- demo/react/vite.config.ts
+++ demo/react/vite.config.ts
@@ -2,9 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react()
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
